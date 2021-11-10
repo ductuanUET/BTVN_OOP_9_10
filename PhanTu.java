31f7222b18c8d00f838c6b2e416be9b1cad9f092
Nguyen Duc Tuan <blgameoff@gmail.com>
2021-11-10 12:49:55 +0700

    Update PhanTu.java

--- PhanTu.java
+++ PhanTu.java
@@ -1,1 +1,19 @@
+public class PhanTu<T extends Comparable<T>> {
+    private T obj;
+
+    public  PhanTu() {
+
+    }
+
+    public T getObj() {
         return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+
+    public boolean isGreaterThan(T a) {
+        return this.obj.compareTo(a) > 0;
+    }
+}
